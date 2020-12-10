3397e096898e40534bffb5c3e9abb020e25e287d
Tim <tim.oest@live.nl>
2020-12-10 21:21:27 +0100

    raw skeleton

---
+++ cats_vs_dogs.py
@@ -0,0 +1,24 @@
+# python project two = classifying cats vs dogs with machine learning
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+
+
+# steps:
+
+# DATA
+# -reference and store petimages in a variable
+# -loop through pet images by finding a way to reference the Cat folder and the Dog folder respectively. anything in the Cat folder gets the label 0, Dogs get label 1
+# -make sure all the images are the same size by hardcoding the size to e.g. 50 by 50
+# -shuffle the data
+# -assign variable X to all the attributes besides the label(the features), assign variable to all the labels of the data
+# -figure out how to format this data in order to pass it onto the algorithm that will classify the data
+
+# ALGORITHM
+# -fit the algorithm with the data (train), using the X_train and y_train data
+# -predict the list of all the features, compared to the actual label values. a.k.a. use the predict method in which you pass the X_test and y_test
+# 
+
+
+
