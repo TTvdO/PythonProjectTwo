6caa4b8888b527e7e13e97ce44dd2e702456103f
Tim <tim.oest@live.nl>
2020-12-11 11:16:48 +0100

    loading image arrays into numpy array

--- cats_vs_dogs.py
+++ cats_vs_dogs.py
@@ -1,10 +1,13 @@
 # python project two = classifying cats vs dogs with machine learning
-import tensorflow as tf
+# import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 import os
 import cv2
+from PIL import Image
+from matplotlib import image
+from matplotlib import pyplot
 
 
 # steps:
@@ -24,6 +27,14 @@ import cv2
 # -figure out what it is exactly that you want to use as your features. I guess all of the pixels of the 50 by 50 pixels. 
 # -add the label as a last element OR create a dictionary containing a list of the features as its first element 
 # -find a way to display any of the images on your screen (catImages[0] for example)
+
+# catImage = image.imread(f'C:/Users/Tim/Desktop/PetImages/Cat/0.jpg')
+# print(catImage.dtype)
+# print(catImage.shape)
+# pyplot.imshow(catImage)
+# pyplot.show()
+
+
 catImagesFolder = os.listdir("C:/Users/Tim/Desktop/PetImages/Cat")
 dogImagesFolder = os.listdir("C:/Users/Tim/Desktop/PetImages/Dog")
 
@@ -33,33 +44,38 @@ dogImages = []
 counter = 0
 
 for catImage in catImagesFolder[:-1]:
-    catImage = cv2.imread(f'{counter}.jpg')
     try:
-        catImageResized = cv2.resize(catImage, (48,48), interpolation=cv2.INTER_AREA)
-        if counter == 0:
-            print(catImageResized.shape())
+        # image.imread returns a numpy array. (M,N) for grayscale images, (M,N,3) for rgb images
+        catImage = image.imread(f'C:/Users/Tim/Desktop/PetImages/Cat/{counter}.jpg')
+        catImage = cv2.cvtColor(catImage, cv2.COLOR_RGB2GRAY)
+        catImageResized = cv2.resize(catImage, (48,48))
+        # if counter == 0:
+        #     print(catImageResized.dtype)
+        #     print(catImageResized.shape)
+            # pyplot.imshow(catImageResized)
+            # pyplot.show()
         catImages.append(catImageResized)
     except Exception as e:
         pass
         # print(str(e))
     counter += 1
-counter = 0
-for dogImage in dogImagesFolder[:-1]:
-    catImage = cv2.imread(f'{counter}.jpg')
-    try:
-        dogImageResized = cv2.resize(dogImage, (48,48), interpolation=cv2.INTER_AREA)
-        if counter == 0:
-            print(dogImageResized.shape())
-        dogImages.append(dogImageResized)
-    except Exception as e:
-        pass
-        # print(str(e))
-    counter += 1
+# counter = 0
+# for dogImage in dogImagesFolder[:-1]:
+#     catImage = cv2.imread(f'{counter}.jpg')
+#     try:
+#         dogImageResized = cv2.resize(dogImage, (48,48), interpolation=cv2.INTER_AREA)
+#         if counter == 0:
+#             print(dogImageResized.shape())
+#         dogImages.append(dogImageResized)
+#     except Exception as e:
+#         pass
+#         # print(str(e))
+#     counter += 1
 
-catImages = np.array(dogImages)
-dogImages = np.array(dogImages)
+catImages = np.array(catImages)
+# dogImages = np.array(dogImages)
 
-# print(catImages)
+print(catImages)
 # print(dogImages)
 
 # ALGORITHM
