002b00e8b003248cd1d509406c9f110f61f77f47
Tim <tim.oest@live.nl>
2020-12-11 19:32:49 +0100

    yee

--- cats_vs_dogs.py
+++ cats_vs_dogs.py
@@ -14,22 +14,15 @@ import pickle
 # steps:
 
 # DATA
-# -reference and store petimages folder in a variable
-# -loop through pet images by finding a way to reference the Cat folder and the Dog folder respectively. anything in the Cat folder gets the label 0, Dogs get label 1
+# -[X]reference and store petimages folder in a variable
+# -[X]loop through pet images by finding a way to reference the Cat folder and the Dog folder respectively. anything in the Cat folder gets the label 0, Dogs get label 1
     # p.s. you need to add to the already existing path to the petimages folder here. you'd need to add /Cat and /Dog respectively
-# -make sure all the images are the same size by hardcoding the size to e.g. 50 by 50, just execute this step at the beginning of the loop
-# -shuffle the data
-# -assign variable X to all the attributes besides the label(the features), assign variable to all the labels of the data
+# -[X]make sure all the images are the same size by hardcoding the size to e.g. 50 by 50, just execute this step at the beginning of the loop
+# -[X]shuffle the data
+# -[X]assign variable X to all the attributes besides the label(the features), assign variable to all the labels of the data
 # -figure out how to format this data in order to pass it onto the algorithm that will classify the data
 
-# temp:
-# ok so I have the images in lists. now I want to:
-# -transform all these images to the same size, probably before adding them to the list 
-# -figure out what it is exactly that you want to use as your features. I guess all of the pixels of the 50 by 50 pixels. 
-# -add the label as a last element OR create a dictionary containing a list of the features as its first element 
-# -find a way to display any of the images on your screen (catImages[0] for example)
-
-# get the cat and dog images out of the pickle state here
+# get the X data (features/input data) and y data (labels/targets) out of the pickle state here
 with open('Xy', 'rb') as f:
     X, y = pickle.load(f)
 
