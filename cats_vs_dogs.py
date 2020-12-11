81b765616e57b1818d0488268940bf6532c509f1
Tim <tim.oest@live.nl>
2020-12-11 14:56:15 +0100

    loading cat and dog images properly

--- cats_vs_dogs.py
+++ cats_vs_dogs.py
@@ -29,54 +29,20 @@ import pickle
 # -add the label as a last element OR create a dictionary containing a list of the features as its first element 
 # -find a way to display any of the images on your screen (catImages[0] for example)
 
-catImagesFolder = os.listdir("C:/Users/Tim/Desktop/PetImages/Cat")
-dogImagesFolder = os.listdir("C:/Users/Tim/Desktop/PetImages/Dog")
+# get the cat and dog images out of the pickle state here
+with open('catAndDogImages', 'rb') as f:
+    catImages, dogImages = pickle.load(f)
 
-catImages = []
-dogImages = []
+# catsWithLabels = {}
+# dogsWithLabels = {}
 
-counter = 0
+# for catImage in catImages:
+#     catsWithLabels.append(catImage, 0)
+# for dogImage in dogImages:
+#     dogsWithLabels.append(dogImage, 1)
 
-for catImage in catImagesFolder[:-1]:
-    try:
-        catImage = image.imread(f'C:/Users/Tim/Desktop/PetImages/Cat/{counter}.jpg')
-        catImage = cv2.cvtColor(catImage, cv2.COLOR_RGB2GRAY)
-        catImageResized = cv2.resize(catImage, (48,48))
-        catImages.append(catImageResized)
-    except Exception as e:
-        pass
-    counter += 1
-counter = 0
-for dogImage in dogImagesFolder[:-1]:
-    try:
-        dogImage = image.imread(f'C:/Users/Tim/Desktop/PetImages/Dog/{counter}.jpg')
-        dogImage = cv2.cvtColor(dogImage, cv2.COLOR_RGB2GRAY)
-        dogImageResized = cv2.resize(dogImage, (48,48))
-        dogImages.append(dogImageResized)
-    except Exception as e:
-        pass
-    counter += 1
-
-catImages = np.array(catImages)
-dogImages = np.array(dogImages)
-
-with open('catAndDogImages', 'wb') as f:
-    pickle.dump([catImages, dogImages], f)
-
-# get the cat and dog images out of the pickle state
-catImages = []
-dogImages = []
-
-catsWithLabels = {}
-dogsWithLabels = {}
-
-for catImage in catImages:
-    catsWithLabels.append(catImage, 0)
-for dogImage in dogImages:
-    dogsWithLabels.append(dogImage, 1)
-
-print(catsWithLabels[:5])
-print(dogsWithLabels[:5])
+# print(catsWithLabels[:5])
+# print(dogsWithLabels[:5])
 
 
 # print("cats", catImages)
