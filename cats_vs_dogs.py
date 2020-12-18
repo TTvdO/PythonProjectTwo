dc2542e80df1d3eb24ae4245a84e8687d2211185
Tim <tim.oest@live.nl>
2020-12-18 12:45:07 +0100

    wipwip

--- cats_vs_dogs.py
+++ cats_vs_dogs.py
@@ -10,17 +10,9 @@ from matplotlib import image
 from matplotlib import pyplot
 import pickle
 from sklearn.model_selection import train_test_split #, cross_validate
-
-# steps:
-
-# DATA
-# -[X]reference and store petimages folder in a variable
-# -[X]loop through pet images by finding a way to reference the Cat folder and the Dog folder respectively. anything in the Cat folder gets the label 0, Dogs get label 1
-    # p.s. you need to add to the already existing path to the petimages folder here. you'd need to add /Cat and /Dog respectively
-# -[X]make sure all the images are the same size by hardcoding the size to e.g. 50 by 50, just execute this step at the beginning of the loop
-# -[X]shuffle the data
-# -[X]assign variable X to all the attributes besides the label(the features), assign variable to all the labels of the data
-# -[X]split your X and y into X_test, y_test, X_train, y_train
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import tensorflow as tf
 
 # get the X data (features/input data) and y data (labels/targets) out of the pickle state here
 with open('Xy', 'rb') as f:
@@ -29,17 +21,53 @@ with open('Xy', 'rb') as f:
 # split the data up into training and testing data (in sample & out of sample data)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# now you have the data that you need. now it's time to figure out:
-# -[]how to pass this data properly to an algorithm
-        # Xy is a numpy array filled with arrays right now. depending on the algorithm you're going to use, you might need to transform this data into a single array, 
-        # but you'd need to know which algorithm you're using first and find out how it wants to receive the data through scikit-learn docs and stackoverflow threads
-# -[X]which algorithm to select for training:
-    # Convolutional Neural Networks are the correct choice for image classification problems
-    # TODO: now to find out in which form this algorithm wants its data. I think it wants it as one big array, so you'd need to just put all the arrays together
-    # for every single 2d array, and then move on to the stuff listed under algorithm 
+# so we can access the X_test and y_test data in our prediction class later. we need the same randomly chosen X_test and y_test values, because the algorithm
+# shouldn't have already seen the images that we are testing it on
+with open('X_train, X_test, y_train, y_test', 'wb') as f:
+    pickle.dump([X_train, X_test, y_train, y_test], f)
 
-# ALGORITHM
-# -create the model outline
-# -fit the algorithm with the data (train), using the X_train and y_train data
-# -predict the list of all the features, compared to the actual label values. a.k.a. use the predict method in which you pass the X_test and y_test
-# 
+# make sequential model, which we can add layers to and thus create the convolutional neural network we need to classify images
+model = Sequential()
+
+# layer 1, input layer
+# 1. first argument Conv2D: filters
+# 2. second argument Conv2D: kernel size. must be a tuple of odd integers with the same values. if input image > 128x128, consider (5,5) and (7,7)
+#       if input image < 128x128, consider (1,1) and (3,3)
+# 3. input_shape, which are the WxH of the image together with the amount of numbers representing each pixel (e.g. 3 for RGB images, 1 for grayscale images)
+model.add(Conv2D(32, (3, 3), input_shape = (48,48,1)))
+# activation function "rectified linear" does this: negative number as input -> return 0 as output. positive number -> return the actual number as output
+# even though all the values of pixels aren't going to be negative, even when divided by 255, the nodes in each layer of the neural network will have not only
+# the inputs, but also their own randomly initialized weights. the inputs will be multiplied by these weights and could make your output negative. 
+# to avoid this we use this rectified linear activation function that returns 0 as output for any negative number
+model.add(Activation("relu"))
+# max pooling helps reduce the size of the output volume. can also be achieved by increasing an optional parameter of Conv2D 'strides' to (2,2)
+# maxpooling converges 2 values into 1. images are a unique situation where this is acceptable, since pixels one to the right or left will often be roughly the same.
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+# layer 2
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+# Flatten converts the 3D feature maps to 1D feature vectors, which is necessary for the next dense layer that only works with 1D data
+model.add(Flatten())
+
+# layer 3
+# dense layer just states: this is how many inputs we are expecting for this layer. it will use (input * weight) for the output value of every single node
+model.add(Dense(64))
+model.add(Activation("relu"))
+
+# layer 4, output layer
+model.add(Dense(1))
+# return value between 0 and 1 as an output. e.g. value of .74 for a classification problem means the algorithm thinks the thing to predict was a 1 as class most likely.
+# you'd just convert the prediction to an integer at the time of predicting new images with this model to get a 0 or 1
+model.add(Activation("sigmoid"))
+
+# compile, which readies the model for training
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+
+# X = tf.ragged.constant(X).to_tensor()
+# y = tf.ragged.constant(y).to_tensor()
+model.fit(X_train, y_train, batch_size=32, epochs=3, validation_split=0.1)
+
+model.save('ConvolutionalNeuralNetworkV1.model')
--- load_images_and_save.py
+++ load_images_and_save.py
@@ -17,14 +17,13 @@ petImages = []
 
 counter = 0
 
-# hier al doen:
-# -gemixte lijst van beide cat en dog images, die je daarna nog shuffled[x]
-# -aparte lijsten voor X en y scheiden van elkaar
 for catImage in catImagesFolder[:-1]:
     try:
         catImage = image.imread(f'C:/Users/Tim/Desktop/PetImages/Cat/{counter}.jpg')
         catImage = cv2.cvtColor(catImage, cv2.COLOR_RGB2GRAY)
         catImageResized = cv2.resize(catImage, (48,48))
+        catImageResized = catImageResized.reshape(-1)
+        catImageResized = np.array(catImageResized)
         petImages.append([0, catImageResized])
     except Exception as e:
         pass
@@ -35,6 +34,8 @@ for dogImage in dogImagesFolder[:-1]:
         dogImage = image.imread(f'C:/Users/Tim/Desktop/PetImages/Dog/{counter}.jpg')
         dogImage = cv2.cvtColor(dogImage, cv2.COLOR_RGB2GRAY)
         dogImageResized = cv2.resize(dogImage, (48,48))
+        dogImageResized = dogImageResized.reshape(-1)
+        dogImageResized = np.array(dogImageResized)
         petImages.append([1, dogImageResized])
     except Exception as e:
         pass
@@ -42,12 +43,11 @@ for dogImage in dogImagesFolder[:-1]:
 petImages = np.array(petImages)
 np.random.shuffle(petImages)
 print(petImages[1])
-X = petImages[:,0]
-print(X[1])
-y = petImages[:,1]
-print(y[1])
-# catImages = np.array(catImages)
-# dogImages = np.array(dogImages)
+X = petImages[:,1]
+# X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+X = X/255.0
 
-with open('Xy2', 'wb') as f:
+y = petImages[:,0]
+
+with open('Xy', 'wb') as f:
     pickle.dump([X, y], f)
