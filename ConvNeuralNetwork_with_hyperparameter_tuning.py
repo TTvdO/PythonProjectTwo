a12bfd9e5a33c4e8659c0224f9029bb0fb604eab
Tim <tim.oest@live.nl>
2020-12-22 17:22:14 +0100

    cleanup

--- ConvNeuralNetwork_with_hyperparameter_tuning.py
+++ ConvNeuralNetwork_with_hyperparameter_tuning.py
@@ -1,5 +1,3 @@
-# python project two = classifying cats vs dogs with machine learning
-# import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
@@ -9,10 +7,9 @@ from PIL import Image
 from matplotlib import image
 from matplotlib import pyplot
 import pickle
-from sklearn.model_selection import train_test_split #, cross_validate
+from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
-# from tensorflow.keras.activations import relu, elu
 import tensorflow as tf
 import talos
 
@@ -32,14 +29,6 @@ params = {
 }
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-print(len(X_train))
-print(len(y_train))
-
-# np.save('X_test.npy', X_test)
-# np.save('y_test.npy', y_test)
-
-# model.save('ConvolutionalNeuralNetworkV2.model')
 
 def catsvsdogs(x_train2, y_train2, x_test2, y_test2, params):
     model = Sequential()
@@ -63,10 +52,8 @@ def catsvsdogs(x_train2, y_train2, x_test2, y_test2, params):
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # out = model.fit(X_train, y_train, batch_size=32, epochs=10)
-    out = model.fit(x_train2, y_train2, batch_size=params['batch_size'], epochs=3)
+    out = model.fit(x_train2, y_train2, batch_size=params['batch_size'], epochs=15)
 
     return out, model
 
-# t = talos.Scan(x=X, y=y, params=params, model=catsvsdogs, experiment_name='test_talos')
-# t = talos.Scan(x=X_train, y=y_train, params=params, model=catsvsdogs, experiment_name='test_talos2', x_val=X_test, y_val=y_test, val_split=0.0, round_limit=10)
-t = talos.Scan(x=X, y=y, params=params, model=catsvsdogs, experiment_name='test_talos2')
+t = talos.Scan(x=X, y=y, params=params, model=catsvsdogs, experiment_name='test_talos3')
